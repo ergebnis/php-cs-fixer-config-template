ddd6b3e2eddd7de0d807d6abc267df10f84af65c
Andreas Möller <am@localheinz.com>
2025-07-16 10:35:48 +0200

    Fix: Disable newly added fixer

--- src/RuleSet/Custom.php
+++ src/RuleSet/Custom.php
@@ -180,6 +180,7 @@ final class Custom
                 'no_useless_concat_operator' => false,
                 'no_useless_else' => false,
                 'no_useless_nullsafe_operator' => false,
+                'no_useless_printf' => false,
                 'no_useless_return' => false,
                 'no_useless_sprintf' => false,
                 'no_whitespace_before_comma_in_array' => false,
